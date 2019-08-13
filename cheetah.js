2916533ef7b23c549f5385b3b3b8a613850c98a1
cocoelizabeth <cocolovesspinach@gmail.com>
2019-08-13 17:15:37 -0400

    setup file structure, set up webpack, create initial html layout with elements, temporary graphics, kitten ship  is moving across the page, random word being generated with each refresh

---
+++ cheetah.js
@@ -0,0 +1,18 @@
+class Cheetah {
+    constructor(props) {
+        this.pos = props.pos
+    }
+
+    update()
+    clear()
+    draw()
+}
+
+
+///
+
+// dictionary of words
+// class cats
+// class dogs
+// class waves
+// class game (logic of event)
---
+++ notes.js
@@ -0,0 +1,27 @@
+// OBJECT ORIENTED PROGRAMMING
+
+// classes
+
+// constructor function - make first letter capital to show that it is a constructor
+
+function NBAPlayer(name,team, pos) {
+    this.name = name;
+    this.team = team;
+    this.pos = pos;
+}
+
+// instance method: all instances of the class can access this function
+
+NBAPlayer.prototype.dunk = function () {
+
+};
+
+
+// Create an instance and save to a variable
+// sets curry's prototype to NBAPlayer: 
+// curry__proto__ = NBAPlayer
+const curry = new NBAPlayer("steph curry", "warriors", "point guard");
+
+// method style invocation. 
+// 'this' is bound to 'curry' because it is called directly on curry
+curry.dunk();
