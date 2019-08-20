1390114a14e2dc6c0b9fe77bfa885f8362802475
cocoelizabeth <cocolovesspinach@gmail.com>
2019-08-19 21:10:35 -0400

    fix stuff

--- src/js/kitten.js
+++ src/js/kitten.js
@@ -10,7 +10,7 @@ class Kitten {
 
     update(i) {
 
-        this.kittenPos[0]++
+        this.kittenPos[0] += 1.5;
         if (this.active === false) {
             this.fly();
             // this.activeKittens.splice(i, 1);
@@ -26,20 +26,24 @@ class Kitten {
     }
 
     fly() {
-        this.kittenPos[0] += 5;
+        this.kittenPos[0] -= 5;
         this.kittenPos[1] -= 5;
     }
 
     draw(ctx) {
         // debugger
         ctx.drawImage(this.kittenImage, ...this.kittenPos);
-        ctx.font = "16px Poppins, sans-serif";
+        ctx.font = "15px Poppins, sans-serif";
         //  ctx.font = "16px Anton, sans-serif";
         // ctx.font = "16px Jura, sans-serif";
         // ctx.font-family ="UnifrakturCook, sans-serif";
         // ctx.font-size="16px";
         ctx.fillStyle = "white";
-        ctx.fillText(this.word, (this.kittenPos[0] + 30), (this.kittenPos[1] + 144));
+        ctx.shadowColor = '#565656';
+        ctx.shadowBlur = 10;
+        ctx.shadowOffsetX = 3;
+        ctx.shadowOffsetY = 3;
+        ctx.fillText(this.word, (this.kittenPos[0] + 20), (this.kittenPos[1] + 144));
     }
 }
 
