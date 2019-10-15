ae677c2b80d7946482f2103481a81e3c67ad9714
cocoelizabeth <cocolovesspinach@gmail.com>
2019-10-15 12:13:18 -0400

    annotate kitten.js, remove unused code and comments

--- src/js/kitten.js
+++ src/js/kitten.js
@@ -1,65 +1,38 @@
-import Coin from "./coin";
-
-// let lives = 3;
-// const livesDisplay = document.querySelector('#lives');
-
 class Kitten {
-    constructor(kittenPos, word, kittenImage, activeKittens = [], currentWords = [], ctx, lives) {
+    constructor(kittenPos, word, kittenImage, ctx, lives, activeKittens = [], currentWords = []) {
         this.kittenPos = kittenPos;
         this.word = word;
+        this.ctx = ctx;
+        this.lives = lives;
         this.kittenImage = kittenImage;
         this.activeKittens = activeKittens;
         this.currentWords = currentWords;
         this.active = true;
-        this.ctx = ctx;
-        this.lives = lives;
     }
 
     update(i) {
-
-        // if (this.active === false) {
-
-        //     this.kittenPos[0] = 9000;
-        // //    this.fly();
-          
-        //     return false;
-          
-
-        // }
-
+        // move the kitten to the right
         this.kittenPos[0] += 1.5;
-    
+        // remove kitten & their respective word from active kittens & words list 
+        // if they reach the shore or the player correctly types their word
         if (this.kittenPos[0] > 800 || this.active === false) {
             let i = this.activeKittens.indexOf(this);
             this.activeKittens.splice(i, 1);
             this.currentWords.splice(i, 1);
-          
+    
             return true;
         }
         return false;
     }
-
-
-    // hideKittenImage() {
-    //     let image = this.kittenImage = new Image();
-    //     this.kittenImage.src = "https://game-seeds.s3.amazonaws.com/pirate-outline-height-130.png"
-    // }
 
     fly() {
         this.kittenPos[0] -= 5;
         this.kittenPos[1] -= 5;
     }
 
-  
-
     draw(ctx) {
-        // debugger
         ctx.drawImage(this.kittenImage, ...this.kittenPos);
         ctx.font = "13px Poppins, sans-serif";
-        //  ctx.font = "16px Anton, sans-serif";
-        // ctx.font = "16px Jura, sans-serif";
-        // ctx.font-family ="UnifrakturCook, sans-serif";
-        // ctx.font-size="16px";
         ctx.fillStyle = "white";
         ctx.shadowColor = '#565656';
         ctx.shadowBlur = 10;
@@ -70,57 +43,3 @@ class Kitten {
 }
 
 export default Kitten;
-
-
-// OLD
-
-// class Kitten {
-//     constructor(kittenPos, word, kittenImage, activeKittens=[], currentWords=[]) {
-//         this.kittenPos = kittenPos;
-//         this.word = word;
-//         this.kittenImage = kittenImage;
-//         this.activeKittens = activeKittens;
-//         this.currentWords = currentWords;
-//         this.active = true;
-//     }
-
-//     update(i) {
-
-//         this.kittenPos[0] += 1.5;
-//         if (this.active === false) {
-//             this.fly();
-//             // this.activeKittens.splice(i, 1);
-            
-//         }
-//         if (this.kittenPos[0] > 800) {
-//             let i = this.activeKittens.indexOf(this);
-//             this.activeKittens.splice(i, 1);
-//             this.currentWords.splice(i, 1);
-//             return true;
-//         }
-//         return false;
-//     }
-
-//     fly() {
-//         this.kittenPos[0] -= 5;
-//         this.kittenPos[1] -= 5;
-//     }
-
-//     draw(ctx) {
-//         // debugger
-//         ctx.drawImage(this.kittenImage, ...this.kittenPos);
-//         ctx.font = "15px Poppins, sans-serif";
-//         //  ctx.font = "16px Anton, sans-serif";
-//         // ctx.font = "16px Jura, sans-serif";
-//         // ctx.font-family ="UnifrakturCook, sans-serif";
-//         // ctx.font-size="16px";
-//         ctx.fillStyle = "white";
-//         ctx.shadowColor = '#565656';
-//         ctx.shadowBlur = 10;
-//         ctx.shadowOffsetX = 3;
-//         ctx.shadowOffsetY = 3;
-//         ctx.fillText(this.word, (this.kittenPos[0] + 20), (this.kittenPos[1] + 143));
-//     }
-// }
-
-// export default Kitten;
